7e803eaf971dc8367f8445c62e779933f9d5c331
ERIC KELLY <eakelly220@gmail.com>
2018-04-24 21:41:39 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,9 +4,15 @@
 # or whitespace (' ').
 
 class EmailParser 
-  attr_accessor 
+  attr_accessor :emails
   
   def initialize(emails)
-    
+    @emails = emails
+  end 
+  
+  def parse
+    @emails.split(" ").collect do |value| 
+      value.split(",")
+    end 
   end 
 end 
